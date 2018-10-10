4a1d73da0ffce71d8118372d0770529beaa769e4
davidAngulo <davidangulocorcuera@gmail.com>
2018-10-10 14:20:31 +0200

    cell for no data in actors added!

---
+++ Series/ViewController/Cells/EmptyStatusCell/EmptyStatusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmptyStatusTableViewCell.swift
+//  Series
+//
+//  Created by DAVID ANGULO CORCUERA on 10/10/18.
+//  Copyright © 2018 DAVID ANGULO CORCUERA. All rights reserved.
+//
+
+import UIKit
+
+class EmptyStatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
